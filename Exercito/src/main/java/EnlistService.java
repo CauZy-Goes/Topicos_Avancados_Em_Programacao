ea5907ecb6eb60d000b7bbc5a5bfaaf3350cc650
CauZy-Goes <cauafariasdev@gmail.com>
2025-08-16 09:54:56 -0300

    feat: create Enlist service

--- Exercito/src/main/java/EnlistService.java
+++ Exercito/src/main/java/EnlistService.java
@@ -1,6 +1,9 @@
+import java.util.Objects;
+
 public class EnlistService {
 
     public static Boolean needEnlist(String gender, Integer age) {
-            return false;
+
+            return (Objects.equals(gender, "M") && age == 18 );
     }
 }
