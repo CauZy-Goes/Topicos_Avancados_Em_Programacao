43bd493fb6f96fe47fdfcc6864a2055b5811ac46
CauZy-Goes <cauafariasdev@gmail.com>
2025-08-16 10:01:12 -0300

    feat: implemented EnlistService in main

--- Exercito/src/main/java/Main.java
+++ Exercito/src/main/java/Main.java
@@ -4,7 +4,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite sua idade");
+        Integer age = sc.nextInt();
+        sc.nextLine();
 
 
+        System.out.println("Digite seu sexo: 'M' ou 'F'");
+        String gender = sc.nextLine();
+
+
+        System.out.println(
+                EnlistService.needEnlist(gender, age)
+                        ? "Você deve se alistar no exército"
+                        : "Você não precisa se alistar"
+        );
     }
 }
