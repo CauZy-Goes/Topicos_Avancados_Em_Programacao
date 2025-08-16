c2eebf556a9bafe09c3304bd37978ea6e283ea23
CauZy-Goes <cauafariasdev@gmail.com>
2025-08-16 09:03:47 -0300

    chore: add Employee

--- Salary/src/main/java/Main.java
+++ Salary/src/main/java/Main.java
@@ -1,6 +1,30 @@
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Mundo");
+
+        List<Employee> employees = List.of(
+                new Employee("João", 30, 3500.0),
+                new Employee("Maria", 25, 4200.0),
+                new Employee("Carlos", 40, 5800.0)
+        );
+
+
+
+
+//        employees.forEach(System.out::println);
+//
+//        employees.stream()
+//                .forEach(System.out::println);
+//
+//        employees.stream()
+//                .map(Employee::toString)
+//                .forEach(System.out::println);
+//
+//        employees.stream()
+//                .map(Employee::getName)
+//                .forEach(System.out::println);
+
     }
 }
