17dc38fd7bf418b61d8d6d92c5a780c9761c7c85
CauZy-Goes <cauafariasdev@gmail.com>
2025-08-18 21:48:09 -0300

    feat: create String Service

--- Exercicio/src/main/java/ComplexidadeExercicio2/StringService.java
+++ Exercicio/src/main/java/ComplexidadeExercicio2/StringService.java
@@ -16,26 +16,28 @@ public class StringService {
         return lastPosition + 1;
     }
 
-//    public static Integer MyIndexOf(String [] sentence, String [] window){
-//        Integer windowPosition = null;
-//        int counterSentence = 0;
-//
-//        while ( MyLength(sentence) != counterSentence ){
-//            int counterWindow = 0;
-//            while ( MyLength(window) != counterWindow ){
-//                if(window[counterWindow].equals(sentence[counterSentence])){
-//
-//                    if(counterWindow == window.length - 1){}
-//                } else {
-//                    break;
-//                }
-//                counterWindow ++;
-//            }
-//            counterSentence ++;
-//            }
-//
-//
-//    }
-//  return windowPosition;
-//}
+    public static Integer MyIndexOf(String [] sentence, String [] window){
+        Integer windowPosition = null;
+        int counterSentence = 0;
+
+        while ( MyLength(sentence) != counterSentence - 1){
+            int counterWindow = 0;
+            while ( MyLength(window) != counterWindow - 1 ){
+                if(window[counterWindow].equals(sentence[counterSentence])){
+
+                    if(counterWindow == window.length){
+                        windowPosition = counterSentence - counterWindow;
+                    }
+                } else {
+                    break;
+                }
+                counterWindow ++;
+            }
+            counterSentence ++;
+            }
+
+        return windowPosition;
+    }
+
 }
+
